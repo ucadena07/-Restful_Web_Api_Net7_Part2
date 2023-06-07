87b39b0bbf3dccfc23690f151317cfe8740bf47c
Ulises Cadena <ucadena07@gmail.com>
2023-06-07 06:58:18 -0500

    initial commit

---
+++ MagicVilla_Utility/SD.cs
@@ -0,0 +1,15 @@
+﻿namespace MagicVilla_Utility
+{
+    public static class SD
+    {
+        public enum ApiType
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE
+        }
+        public static string SessionToken = "JWTToken";
+        public static string CurrentAPIVersion = "v2";
+    }
+}
