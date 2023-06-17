e200b922c7a309e79aaef14e68614517bd834031
Ulises Cadena <ucadena07@gmail.com>
2023-06-17 07:48:20 -0500

    clean code & extension

---
+++ MagicVilla_VillaAPI/Extensions/CustomExceptionExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Newtonsoft.Json;
+
+namespace MagicVilla_VillaAPI.Extensions
+{
+    public static class CustomExceptionExtensions
+    {
+        public static void HandleError(this IApplicationBuilder app, bool IsDevelopment)
+        {
+            app.UseExceptionHandler(err =>
+            {
+                err.Run(async context =>
+                {
+                    context.Response.StatusCode = 500;
+                    context.Response.ContentType = "application/json";
+                    var feature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (feature != null)
+                    {
+                        if (IsDevelopment)
+                        {
+                            await context.Response.WriteAsync(JsonConvert.SerializeObject(new
+                            {
+                                StatusCode = context.Response.StatusCode,
+                                ErrorMessage = feature.Error.Message,
+                                StackTrace = feature.Error.StackTrace
+                            }));
+                        }
+                        else
+                        {
+                            await context.Response.WriteAsync(JsonConvert.SerializeObject(new
+                            {
+                                StatusCode = context.Response.StatusCode,
+                                ErrorMessage = "Hello from program.cs"
+                            }));
+                        }
+
+                    }
+
+                });
+            });
+        }
+    }
+}
--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -17,6 +17,7 @@ using Swashbuckle.AspNetCore.SwaggerGen;
 using MagicVilla_VillaAPI.Filters;
 using Microsoft.AspNetCore.Diagnostics;
 using Newtonsoft.Json;
+using MagicVilla_VillaAPI.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -91,37 +92,7 @@ if (app.Environment.IsDevelopment())
 }
 
 //app.UseExceptionHandler("/ErrorHandling/ProcessError");
-app.UseExceptionHandler(err =>
-{
-    err.Run(async context =>
-    {
-        context.Response.StatusCode = 500;
-        context.Response.ContentType = "application/json";
-        var feature = context.Features.Get<IExceptionHandlerFeature>();
-        if(feature != null)
-        {
-            if (app.Environment.IsDevelopment())
-            {
-                await context.Response.WriteAsync(JsonConvert.SerializeObject(new
-                {
-                    StatusCode = context.Response.StatusCode,
-                    ErrorMessage = feature.Error.Message,
-                    StackTrace = feature.Error.StackTrace
-                }));
-            }
-            else
-            {
-                await context.Response.WriteAsync(JsonConvert.SerializeObject(new
-                {
-                    StatusCode = context.Response.StatusCode,
-                    ErrorMessage = "Hello from program.cs"
-                }));
-            }
-
-        }
-
-    });
-});
+app.HandleError(app.Environment.IsDevelopment());
 
 app.UseStaticFiles();
 app.UseHttpsRedirection();
